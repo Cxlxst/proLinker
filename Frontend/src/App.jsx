e9a2a1b49bca66e2dce92cd61dabb9fd6e5d917c
Cxlxst <celeste.terpin@outlook.com>
2024-10-14 20:09:35 +0200

    add register css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
 import './App.css'
+
 import Register from './pages/Register'
+import Home from './App.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,40 +1,44 @@
 
 export default function Register(){
     return(
-        <div>
-            <div className="column">
-                <div>
-                    <label>Prénom</label>
-                    <input/>
+        <body>
+            <div className="container">
+                <div className="column">
+                    <div>
+                        <label>Prénom</label>
+                        <input/>
+                    </div>
+
+                    <div>
+                        <label>E-mail</label>
+                        <input/>
+                    </div>
+
+                    <div>
+                        <label>Date de naissance</label>
+                        <input/>
+                    </div>
                 </div>
 
+                <div className="column">
                 <div>
-                    <label>E-mail</label>
-                    <input/>
-                </div>
+                        <label>Nom</label>
+                        <input/>
+                    </div>
 
-                <div>
-                    <label>Date de naissance</label>
-                    <input/>
+                    <div>
+                        <label>Numéro de téléphone</label>
+                        <input/>
+                    </div>
+
+                    <div>
+                        <label>Mot de passe</label>
+                        <input/>
+                    </div>
                 </div>
             </div>
 
-            <div className="column">
-            <div>
-                    <label>Nom</label>
-                    <input/>
-                </div>
-
-                <div>
-                    <label>Numéro de téléphone</label>
-                    <input/>
-                </div>
-
-                <div>
-                    <label>Mot de passe</label>
-                    <input/>
-                </div>
-            </div>
-        </div>
+            <button>S'inscrire</button>
+        </body>
     )
 }
