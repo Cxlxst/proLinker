cce289a230f771105e5a642bd5fdf31cea89e799
nathan <106931831+Iceprod8@users.noreply.github.com>
2024-10-15 22:05:42 +0200

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,15 +2,15 @@ import { BackgroundGradientAnimation } from "../components/ui/background-gradien
 
 export default function Home() {
     return (
-        <BackgroundGradientAnimation interactive={false}>
+        <BackgroundGradientAnimation>
+            <div className="relative z-10 w-full h-full flex flex-col items-center justify-center text-white font-bold px-4 text-3xl text-center md:text-4xl lg:text-7xl">
                 <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
                     Chaque CV a une histoire à raconter.<br /> Aidez-le à trouver son auditoire sur <b>ProLink</b>
                 </p>
-                <button onClick={() => console.log('test')} className="cursor-pointer hover:bg-blue-500 hover:text-white p-3 rounded-lg transition duration-300">
-                    M'inscrire
-                </button>
-            <div className="absolute z-[50000] inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
             </div>
-        </BackgroundGradientAnimation >
-    )
+            <button onClick={() => console.log('test')} className="absolute z-50 cursor-pointer hover:bg-blue-500 hover:text-white p-3 rounded-lg transition duration-300" style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} >
+                M'inscrire
+            </button>
+        </BackgroundGradientAnimation>
+    );
 }
