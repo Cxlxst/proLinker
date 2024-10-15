100654f1d18ac381f5529494baca7e09ee538133
nathan <106931831+Iceprod8@users.noreply.github.com>
2024-10-15 17:35:33 +0200

    Fix bug in front

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,18 +1,16 @@
-import shrek from '../assets/shrek.webp';
 import { BackgroundGradientAnimation } from "../components/ui/background-gradient-animation";
 
 export default function Home() {
     return (
-        <BackgroundGradientAnimation>
-            {/* <body>
-                <h2>Chaque CV a une histoire à raconter.<br/>
-                Aidez-le à trouver son auditoire sur <b>ProLink</b></h2>
-
-                <button>M'inscrire</button>
-            </body> */}
-            {/* <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-
-            </div > */}
+        <BackgroundGradientAnimation interactive={true}>
+            <div className="absolute z-[50000] inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+                <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+                    Chaque CV a une histoire à raconter.<br /> Aidez-le à trouver son auditoire sur <b>ProLink</b>
+                </p>
+                <button onClick={() => console.log('test')} className="cursor-pointer hover:bg-blue-500 hover:text-white p-3 rounded-lg transition duration-300">
+                    M'inscrire
+                </button>
+            </div>
         </BackgroundGradientAnimation >
     )
 }
