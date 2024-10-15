3d783d98006bafd8c1cf2ee611015f386eecb18d
Cxlxst <celeste.terpin@outlook.com>
2024-10-15 15:16:33 +0200

    add settings

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
 import './App.css'
 
+import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Settings from './pages/Settings'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,8 +14,10 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/inscription" element={<Register />} />
+          <Route path="/connexion" element={<Login />} />
+          <Route path="/parametres" element={<Settings />} />
         </Routes>
       </Router>
     </>
---
+++ Frontend/src/pages/Settings.jsx
@@ -0,0 +1,54 @@
+export default function Settings(){
+    return(
+        <body>
+            <div className="container">
+                <div className="column">
+                    <div className="row">
+                        <div>
+                            <label>Prénom</label>
+                            <input type="text" placeholder="John"/>
+                        </div>
+
+                        <div>
+                            <label>Nom</label>
+                            <input type="text" placeholder="Doe"/>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div>
+                            <label>E-mail</label>
+                            <input type="email" placeholder="john.doe@example.com"/>
+                        </div>
+
+                        <div>
+                            <label>Numéro de téléphone</label>
+                            <input type="tel" placeholder="0123456789"/>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div>
+                            <label>Date de naissance</label>
+                            <input type="date"/>
+                        </div>
+                    </div>
+                    
+                    <div className="row">
+                        <div>
+                            <label>Mot de passe actuel</label>
+                            <input type="password"/>
+                        </div>
+
+                        <div>
+                            <label>Nouveau mot de passe</label>
+                            <input type="password"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button>Modifier</button>
+        </body>
+    )
+}
