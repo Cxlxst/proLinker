5cee9346810360a53be2d7c76413d1d5b4960bf8
Cxlxst <celeste.terpin@outlook.com>
2024-10-15 16:24:17 +0200

    add css for add/edit cv

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Settings from './pages/Settings'
+import CreateCv from './pages/CreateCv'
+import EditCv from './pages/EditCv'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/inscription" element={<Register />} />
           <Route path="/connexion" element={<Login />} />
+          <Route path="/creer-mon-cv" element={<CreateCv />} />
+          <Route path="/modifier-mon-cv" element={<EditCv />} />
           <Route path="/parametres" element={<Settings />} />
         </Routes>
       </Router>
