20b609e57f9d57dfaa759052f91ac636efe14534
Cxlxst <celeste.terpin@outlook.com>
2024-10-15 15:14:18 +0200

    update rows/columns

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -3,36 +3,40 @@ export default function Register(){
         <body>
             <div className="container">
                 <div className="column">
-                    <div>
-                        <label>Prénom</label>
-                        <input type="text"/>
+                    <div className="row">
+                        <div>
+                            <label>Prénom</label>
+                            <input type="text" placeholder="John"/>
+                        </div>
+
+                        <div>
+                            <label>Nom</label>
+                            <input type="text" placeholder="Doe"/>
+                        </div>
                     </div>
 
-                    <div>
-                        <label>E-mail</label>
-                        <input type="email"/>
+                    <div className="row">
+                        <div>
+                            <label>E-mail</label>
+                            <input type="email" placeholder="john.doe@example.com"/>
+                        </div>
+
+                        <div>
+                            <label>Numéro de téléphone</label>
+                            <input type="tel" placeholder="0123456789"/>
+                        </div>
                     </div>
 
-                    <div>
-                        <label>Date de naissance</label>
-                        <input type="date"/>
-                    </div>
-                </div>
+                    <div className="row">
+                        <div>
+                            <label>Date de naissance</label>
+                            <input type="date"/>
+                        </div>
 
-                <div className="column">
-                <div>
-                        <label>Nom</label>
-                        <input type="text"/>
-                    </div>
-
-                    <div>
-                        <label>Numéro de téléphone</label>
-                        <input type="tel"/>
-                    </div>
-
-                    <div>
-                        <label>Mot de passe</label>
-                        <input type="password"/>
+                        <div>
+                            <label>Mot de passe</label>
+                            <input type="password"/>
+                        </div>
                     </div>
                 </div>
             </div>
